5a6efdb20b546a2006a3aee3fcd88183d1a875ca
zhigang <wang.zhigang@elandsystems.com.cn>
2019-03-21 11:02:33 +0800

    change request PostXML Content-Type to 'text/xml; charset=utf-8'

--- request.go
+++ request.go
@@ -104,7 +104,7 @@ func (r *Requester) PostXML(endpoint string, xml string, responseStruct interfac
 	if err := r.SetCrumb(ar); err != nil {
 		return nil, err
 	}
-	ar.SetHeader("Content-Type", "application/xml")
+	ar.SetHeader("Content-Type", "text/xml; charset=utf-8")
 	ar.Suffix = ""
 	return r.Do(ar, &responseStruct, querystring)
 }
